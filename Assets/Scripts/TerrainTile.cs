afa650c55b5cc3f549c2a6010ea8d705e1568f59
Ryan RC <murphyslaw480@gmail.com>
2014-07-12 13:41:12 -0400

    basic MakeMountain method

--- Assets/Scripts/TerrainTile.cs
+++ Assets/Scripts/TerrainTile.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 public class TerrainTile : MonoBehaviour {
+    // position within tilemap
+    public int Row, Col;
+    public int Elevation;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 }
--- Assets/Scripts/Tilemap.cs
+++ Assets/Scripts/Tilemap.cs
@@ -9,7 +9,7 @@ public class Tilemap : MonoBehaviour {
     /// </summary>
     public GameObject TileObject;
 
-    public int NumRows = 10; 
+    public int NumRows = 10;
     public int NumCols = 10;
 
     GameObject[,] _tiles;
@@ -36,6 +36,7 @@ public class Tilemap : MonoBehaviour {
                 AddTile(row, col, 0);
             }
         }
+        MakeMountain(10, 10, 5, 1);
     }
 
     public void Clear() {
@@ -46,11 +47,35 @@ public class Tilemap : MonoBehaviour {
     }
 
     private void AddTile(int row, int col, int elevation) {
+        // create a new tile as a subobject of the tilemap
         var tile = (GameObject)GameObject.Instantiate(TileObject);
         tile.transform.parent = transform; // tile is a subobject of the Tilemap
+        // position and scale the new tile
         tile.transform.localScale = new Vector3(tileSizeX, (elevation + 1) * tileSizeY, tileSizeZ);
         tile.transform.position = new Vector3(col * tileSizeX, 0, row * tileSizeZ);
+        // give it a name for convenience
         tile.name = string.Format(TileNameFormat, row, col);
+        // tell tile script its position within the tilemap
+        var terrainData = tile.GetComponent<TerrainTile>();
+        terrainData.Row = row;
+        terrainData.Col = col;
+        terrainData.Elevation = elevation;
         _tiles[row, col] = tile;
+    }
+
+    private void MakeMountain(int peakRow, int peakCol, int height, float slope) {
+        int startRow = Mathf.Clamp(peakRow - (int)(height / slope), 0, NumRows);
+        int endRow = Mathf.Clamp(peakRow + (int)(height / slope), 0, NumRows);
+        int startCol = Mathf.Clamp(peakCol - (int)(height / slope), 0, NumCols);
+        int endCol = Mathf.Clamp(peakCol + (int)(height / slope), 0, NumCols);
+        for (int row = 0; row < NumRows; row++) {
+            for (int col = 0; col < NumCols; col++) {
+                float distance = Mathf.Sqrt(Mathf.Pow(row - peakRow, 2) + Mathf.Pow(col - peakCol, 2));
+                int heightAdjustment = Mathf.Max(0, (int)(height - slope * distance)); // no negative adjustments
+                var tile = _tiles[row, col];
+		var oldHeight = tile.transform.localScale.y;
+		tile.transform.localScale = new Vector3(tileSizeX, oldHeight + tileSizeY * heightAdjustment, tileSizeZ);
+            }
+        }
     }
 }
