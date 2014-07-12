3c996038ce7d1d29b6f58c7b87663a6565b91fe0
Ryan RC <murphyslaw480@gmail.com>
2014-07-12 16:31:16 -0400

    randomize mountains

--- Assets/Scripts/Mountains.cs
+++ Assets/Scripts/Mountains.cs
@@ -9,17 +9,25 @@ public class Mountains : TerrainShaper {
         _tiles = tiles;
         _numRows = tiles.GetLength(0);
         _numCols = tiles.GetLength(1);
-        MakeMountain(Row, Col, 5, 1);
+	int minRow = Mathf.Max(0, Row - Range);
+	int maxRow = Mathf.Min(_numRows, Row + Range);
+	int minCol = Mathf.Max(0, Col - Range);
+	int maxCol = Mathf.Min(_numRows, Col + Range);
+        int nMountains = (int)(Density * tiles.LongLength);
+        for (int i = 0; i < nMountains; i++) {
+            int row = Random.Range(minRow, maxRow);
+            int col = Random.Range(minCol, maxCol);
+            MakeMountain(row, col, 5, 1);
+        }
     }
 
     private void MakeMountain(int peakRow, int peakCol, int height, float slope) {
-        Debug.Log(string.Format("{0} making mountain at {1}, {2}. h={3}, s={4}", name, peakCol, peakRow, height, slope));
         int startRow = Mathf.Clamp(peakRow - (int)(height / slope), 0, _numRows);
         int endRow = Mathf.Clamp(peakRow + (int)(height / slope), 0, _numRows);
         int startCol = Mathf.Clamp(peakCol - (int)(height / slope), 0, _numCols);
         int endCol = Mathf.Clamp(peakCol + (int)(height / slope), 0, _numCols);
-        for (int row = 0; row < _numRows; row++) {
-            for (int col = 0; col < _numCols; col++) {
+        for (int row = startRow; row < endRow; row++) {
+            for (int col = startCol; col < endCol; col++) {
                 float distance = Mathf.Sqrt(Mathf.Pow(row - peakRow, 2) + Mathf.Pow(col - peakCol, 2));
                 int heightAdjustment = Mathf.Max(0, (int)(height - slope * distance)); // no negative adjustments
                 var tile = _tiles[row, col];
