a7e4f85b059a0b2d7e1a4971a55a89b82555fa4c
Ryan RC <murphyslaw480@gmail.com>
2014-07-12 15:54:15 -0400

    autoselect tilemap on tile selection

---
+++ Assets/Editor/TerrainTileInspector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(TerrainTile))]
+public class TerrainTileInspector : Editor {
+    /// <summary>
+    /// Selecting any tile within the TileMap should select the tilemap itself
+    /// </summary>
+    public override void OnInspectorGUI() {
+        if (Selection.activeGameObject.GetComponent<TerrainTile>() != null) {
+            Selection.activeGameObject = Selection.activeGameObject.transform.parent.gameObject;
+        }
+    }
+}
