cb2851ab1ad138ce289e00b6d8d8af2a0091ef64
Ryan RC <murphyslaw480@gmail.com>
2014-07-13 16:50:34 -0400

    custom select parent shortcut

---
+++ Assets/Editor/MyShortcuts.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public static class MyShortcuts {
+    const string ShortcutKey = "&p";
+
+    [MenuItem("My Commands/Select Parent " + ShortcutKey)]
+    static void SelectParent() {
+        Selection.activeGameObject = Selection.activeGameObject.transform.parent.gameObject;
+    }
+}
